6f9d0b3390da7ef9a8cadd3147b88e56cab41ab3
QuadieZ <62594692+QuadieZ@users.noreply.github.com>
2020-11-02 01:39:48 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const sc = document.querySelector('section');
+window.addEventListener('scroll',() => {
+    let y = window.scrollY;
+    sc.style.clipPath = "circle(" + y + "px at center)"
+})
+
+let interDate = new Date("Nov 6, 2020").getTime()
+
+let x = setInterval(() => {
+    let now = new Date().getTime();
+    let d = interDate - now;
+
+    let days = Math.floor(d / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((d % (1000 * 60)) / 1000);
+
+    let timer = document.getElementById('timer')
+    timer.innerHTML = days + " Days " + hours + " Hours " + minutes + " Mins " + seconds + " Secs " 
+})
+
+let snd = new Audio("./assets/yay.mp3"); // buffers automatically when created
+snd.play();
+
+let easter = document.getElementById('easter')
+easter.onclick = () => {
+    snd.play();
+}
